6b6b62936fb1338b2fc4c4209bb10770f0377c56
Jeppe Stenstrup Lauridsen <Jeppe.StenstrupLauridsen@vitecsoftware.com>
2023-10-19 13:54:15 +0200

    feat: add EconomicService

---
+++ Services/EconomicService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Subscription_Listing.Services
+{
+    public class EconomicService
+    {
+        private readonly RestHelp _restHelp;
+        
+        public EconomicService()
+        {
+            _restHelp = new RestHelp("demo", "demo"); // Should be extracted and parsed from elsewhere
+        }
+    }
+}
