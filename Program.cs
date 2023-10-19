7f541a422a1cfdc950849b79eecaf653a973acb5
Jeppe Stenstrup Lauridsen <Jeppe.StenstrupLauridsen@vitecsoftware.com>
2023-10-19 14:01:24 +0200

    feat: remove Test from project

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Subscription_Listing.Services;
 
 namespace Subscription_Listing
 {
@@ -10,8 +11,15 @@ namespace Subscription_Listing
     {
         static async Task Main(string[] args)
         {
-            Test t = new Test();
-            await t.Go();
+            var api = new EconomicService();
+
+            var subscriptions = await api.FetchSubscriptions();
+
+            foreach (var subscription in subscriptions)
+            {
+                Console.WriteLine(subscription.name);
+            }
+            
             Console.WriteLine("finished");
             Console.ReadKey();
         }
