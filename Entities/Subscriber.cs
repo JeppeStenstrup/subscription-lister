add8d41667b4cb7b6fe34626b7e86d400509b91d
Jeppe Stenstrup Lauridsen <Jeppe.StenstrupLauridsen@vitecsoftware.com>
2023-10-19 13:55:14 +0200

    feat: add Subscriber entity

---
+++ Entities/Subscriber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Subscription_Listing
+{
+    /// <summary>
+    /// DTO Subscriber
+    /// Extracted from https://apis.e-conomic.com/#Subscriptions..tag/Subscribers
+    /// </summary>
+    public class Subscriber
+    {
+        public int number { get; set; }
+        public int subscriptionNumber { get; set; }
+        public int customerNumber { get; set; }
+        public int yourRef { get; set; }
+        public string otherRef { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public int specialPrice { get; set; }
+        public int discountPercentage { get; set; }
+        public DateTime discountExpiryDate { get; set; }
+        public int priceFactor { get; set; }
+        public DateTime registrationDate { get; set; }
+        public DateTime expiryDate { get; set; }
+        public int projectNumber { get; set; }
+        public int departmentNumber { get; set; }
+        public int bookedInvoiceId { get; set; }
+        public int draftDocumentId { get; set; }
+        public string extraTextForInvoice { get; set; }
+        public string comments { get; set; }
+        public DateTime lastUpdated { get; set; }
+        public string objectVersion { get; set; }
+    }
+}
