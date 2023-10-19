bde2914a182884bb0977b69f0af9259a9e77d2b5
Jeppe Stenstrup Lauridsen <Jeppe.StenstrupLauridsen@vitecsoftware.com>
2023-10-19 13:58:30 +0200

    feat: extract Test.Go() to EconomicService.FetchSubscriptions()

--- Services/EconomicService.cs
+++ Services/EconomicService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Subscription_Listing.Services
 {
@@ -9,6 +11,23 @@ namespace Subscription_Listing.Services
         public EconomicService()
         {
             _restHelp = new RestHelp("demo", "demo"); // Should be extracted and parsed from elsewhere
+        }
+
+        public async Task<List<Subscription>> FetchSubscriptions()
+        {
+            var Subscriptions = new List<Subscription>();
+            
+            string cursor = null;
+            do
+            {
+                // https://apis.e-conomic.com/subscriptionsapi/v2.0.0/subscriptions
+                var datatuple = await _restHelp.GetOpenApiCollectionAsync<Subscription>(RestApi.subscriptionsapi, "v2.0.0", "subscriptions", null, cursor);
+                cursor = datatuple.Item1;
+                Subscriptions.AddRange(datatuple.Item2);
+            }
+            while (cursor != null);
+
+            return Subscriptions;
         }
     }
 }
